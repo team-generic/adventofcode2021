9395748082d9b159f45d70dfd1844bc927e8c4b7
Briana Oursler <briana.oursler@gmail.com>
2021-12-02 12:04:59 -0800

    Update to include second solution and Cargo.toml

--- week1/src/main.rs
+++ week1/src/main.rs
@@ -4,18 +4,29 @@ use std::io::{BufRead, BufReader};
 fn main() -> Result<(), std::io::Error> {
     let filename = std::env::args().nth(1).expect("no filename given");
     println!("{}", filename);
+    let window_size = match std::env::args().nth(2) {
+        Some(x) => x.parse::<usize>().unwrap(),
+        None => 1,
+    };
     let input = File::open(filename)?;
     let reader = BufReader::new(input);
-    let mut count = 0;
+    let count;
+
     let readings: Vec<usize> = reader
         .lines()
         .map(|line| line.unwrap().parse::<usize>().unwrap())
         .collect();
-    for depth in 0..readings.len() - 1 {
-        if readings[depth] < readings[depth + 1] {
+    count = depth_changes(window_size, readings.as_slice());
+    println!("depth changes: {}", count);
+    Ok(())
+}
+
+fn depth_changes(window_size: usize, readings: &[usize]) -> usize {
+    let mut count: usize = 0;
+    for depth in 0..readings.len() - window_size {
+        if readings[depth] < readings[depth + window_size] {
             count += 1;
         }
     }
-    println!("depth changes: {}", count);
-    Ok(())
+    count
 }
