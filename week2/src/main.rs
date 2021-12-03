d1aec9560b78fe61caee621921ec81630253ed25
Briana Oursler <briana.oursler@gmail.com>
2021-12-02 17:17:16 -0800

    Initial solution day 2 puzzle 1

---
+++ week2/src/main.rs
@@ -0,0 +1,59 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Debug)]
+struct Order {
+    cmd: String,
+    magnitude: usize,
+}
+
+#[derive(Debug)]
+struct Position {
+    depth: usize,
+    hz: usize,
+}
+fn main() -> Result<(), std::io::Error> {
+    let filename = std::env::args().nth(1).expect("no filename given");
+    println!("{}", filename);
+    let input = File::open(filename)?;
+    let reader = BufReader::new(input);
+
+    let readings: Vec<Order> = reader
+        .lines()
+        .map(|line| -> Order {
+            let tmp = line.unwrap();
+            let mut iter = tmp.split_whitespace();
+            Order {
+                cmd: match iter.next() {
+                    Some(x) => x.to_string(),
+                    None => "".to_string(),
+                },
+                magnitude: match iter.next() {
+                    Some(x) => x.parse::<usize>().unwrap(),
+                    None => 0,
+                },
+            }
+        })
+        .collect::<Vec<Order>>();
+    println!("result: {}", orders_received(readings.as_slice()));
+    Ok(())
+}
+
+fn orders_received(orders: &[Order]) -> usize {
+    let mut pos = Position { depth: 0, hz: 0 };
+    for order in orders {
+        match order.cmd.as_str() {
+            "forward" => {
+                pos.hz += order.magnitude;
+            }
+            "down" => {
+                pos.depth += order.magnitude;
+            }
+            "up" => {
+                pos.depth -= order.magnitude;
+            }
+            _ => println!("no orders"),
+        }
+    }
+    pos.depth * pos.hz
+}
