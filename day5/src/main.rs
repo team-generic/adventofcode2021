8837449cb813c1756aa426bcbd9f4f2c5c7504f3
Briana Oursler <briana.oursler@gmail.com>
2021-12-05 14:55:52 -0800

    Iterating over line segments and adding to a hashmap.

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::num::ParseIntError;
@@ -13,10 +14,19 @@ fn main() {
     let segments: Vec<LineSegment> = lines_iter
         .map(|line| -> LineSegment { line.parse::<LineSegment>().unwrap() })
         .collect();
+    let mut map = HashMap::new();
+
     println!("{:?}", segments);
+    for segment in segments {
+        for point in segment {
+            println!("point: {}, {}", point.x, point.y);
+            let counter = map.entry(point).or_insert(0);
+            *counter += 1;
+        }
+    }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 struct Point {
     x: isize,
     y: isize,
@@ -51,5 +61,58 @@ impl FromStr for LineSegment {
         let end = coords[2].parse::<Point>()?;
         assert_eq!(coords[1], "->");
         Ok(LineSegment { start, end })
+    }
+}
+
+impl LineSegment {
+    fn iter(&self) -> SegmentIter {
+        SegmentIter {
+            start: self.start,
+            end: self.end,
+            next: Some(self.start),
+        }
+    }
+}
+impl IntoIterator for LineSegment {
+    type Item = Point;
+    type IntoIter = SegmentIter;
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+struct SegmentIter {
+    start: Point,
+    end: Point,
+    next: Option<Point>,
+}
+impl Iterator for SegmentIter {
+    type Item = Point;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut next = match self.next {
+            None => return None,
+            Some(n) => n,
+        };
+        if (self.start.x == self.end.x) ^ (self.start.y == self.end.y) == false {
+            return None;
+        }
+        let ret = Some(next);
+        if next == self.end {
+            self.next = None;
+        } else if self.start.y == self.end.y {
+            if self.start.x > self.end.x {
+                next.x -= 1;
+            } else {
+                next.x += 1;
+            }
+            self.next = Some(next);
+        } else {
+            if self.start.y > self.end.y {
+                next.y -= 1;
+            } else {
+                next.y += 1;
+            }
+            self.next = Some(next);
+        }
+        ret
     }
 }
